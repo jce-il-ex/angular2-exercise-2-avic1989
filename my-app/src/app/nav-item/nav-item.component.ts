6681e9057df0308dc93b49f64f9a3feee99df07b
אבי כהן < >
2017-03-14 10:04:32 +0200

    update2

--- my-app/src/app/nav-item/nav-item.component.ts
+++ my-app/src/app/nav-item/nav-item.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavItemComponent implements OnInit {
 
+myList = [
+{
+  id:1,
+  txt: '1'
+},
+{
+  id:2,
+  txt: '2' 
+},
+{
+  id:3 ,
+  txt: '3'
+},
+{
+  id:4,
+  txt: '4' 
+}
+];
+
   constructor() { }
 
   ngOnInit() {
